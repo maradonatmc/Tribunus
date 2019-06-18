ce27e46d89d28d65a9f9375f70dbb54f8c057394
Renato Ramos <renatoramos89@gmail.com>
2019-06-18 18:26:43 -0300

    Adicionar arquivos de projeto.

---
+++ SalesWebMVC/SalesWebMVC/Pages/Login.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SalesWebMVC.Pages {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login: ContentPage {
+        public Login() {
+            InitializeComponent();
+
+            switch(Device.RuntimePlatform) {
+                case Device.Android:
+                    Padding = new Thickness(10, 20, 10, 10);
+                    break;
+
+                default:
+                    Padding = new Thickness(10);
+                    break;
+            }
+
+            loginButton.Clicked += LoginButton_Clicked;
+        }
+
+        private async void LoginButton_Clicked(object sender, EventArgs e) {
+            if (string.IsNullOrEmpty(emailEntry.Text)) {
+                await DisplayAlert("Erro", "Digite seu e-mail", "Aceitar");
+
+                emailEntry.Focus();
+                return;
+            }
+
+            if (string.IsNullOrEmpty(passwordEntry.Text)) {
+                await DisplayAlert("Erro", "Digite sua senha", "Aceitar");
+
+                passwordEntry.Focus();
+                return;
+            }
+
+            //this.Logar();
+        }
+
+        //private void Logar() {
+            
+        //}
+    }
+}
