21706c0a2a0431fdbf8a52349ad9485cd55d3959
Renato Ramos <renatoramos89@gmail.com>
2019-07-02 13:45:28 -0300

    Lista de Membros

---
+++ Tribunus/Tribunus/Classes/Graduation.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Tribunus.Classes {
+    public class Graduation : ObservableCollection<Members> {
+
+        public string CodGraduacao { get; set; }
+        public string DscGraduacao { get; set; }
+    }
+}
---
+++ Tribunus/Tribunus/Classes/Members.cs
@@ -0,0 +1,9 @@
+﻿namespace Tribunus.Classes {
+    public class Members {
+        public string Apelido { get; set; }
+        public string NomeMembro { get; set; }
+
+        public Members() {
+        }
+    }
+}
